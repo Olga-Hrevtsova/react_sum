b3231776e84c391a46c29a09f8c4bfa06e247cfe
Olga Hrevtsova <nice_fox@ukr.net>
2025-03-24 19:22:22 +0200

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import './App.scss';
 import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <Sum a={2} b={3} />
-    <Sum a={-5} b={5} />
-    <Sum a={10} />
-    <Sum b={5} />
+    <Sum
+    a={2}
+    b={3}
+    />
+
+    <Sum
+    a={-5}
+    b={5}
+    />
+
+    <Sum
+    a={10}
+    />
+
+    <Sum
+    b={5}
+    />
     <Sum />
   </>
 );
